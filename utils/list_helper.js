642d836c0492c011fc0ad13aefec776f338e2095
dzdn <jdustinwilliams@gmail.com>
2022-11-10 21:08:15 -0500

    Complete 4.5 (Add favoriteBlog function and test).

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,17 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+const favorite = (blogs) => {
+
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((prev, current) => {
+        return (prev.likes > current.likes) ? prev : current
+    })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favorite
 }
