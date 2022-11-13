61c6399c4bbc548855bc9e4403955e988591612a
dzdn <jdustinwilliams@gmail.com>
2022-11-13 15:38:29 -0500

    Complete 4.9 (test to verify unique identifier is named 'id').

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -18,6 +18,12 @@ test('correct number of blogs are returned as JSON', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier of blog posts is named id', async () => {
+  const blogId = helper.initialBlogs[0]._id
+  const resultBlog = await api.get(`/api/blogs/${blogId}`)
+  expect(resultBlog.body.id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
